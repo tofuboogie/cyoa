55cc595fe8146aac61c230d7a2036c8a531c2b4e
tofuboogie <tofuboogie@gmail.com>
2016-03-10 14:03:40 -0800

    chmod 6s

--- 2/Rooms_bulk.java
+++ 2/Rooms_bulk.java
@@ -1,37 +1,12 @@
 package dtz.cyoa;
-
 class Rooms_bulk extends Rooms {
 
-/*Another option: start with String[] of Room names, for loop thru each string and add to a HashMap<String,Room>, init room in the map, then use .get(key) to return a room. Loop: 
-Map<String,Room> map = new HashMap<String,Room>;
-for(Map.Entry<String,Room> entry : map.entrySet()) {
-	System.out.println("key=" +entry.getKey() + "value=" + entry.getValue());
-}*/
-
 	public static void initBulkArrays(){
-		tz_rooms = new Room[] {CottonCandyStand, 
-	MiniatureTrainStation, MiniatureTrainCars, ChairSwing, ChairSwingTower, ChairSwingSeat,
-	ChairSwingUnderGrating, ChairSwingTowerMid, ChairSwingTowerNearTop, ChairSwingTowerOnTop, HiStriker,
-	WaterGunGame, DeepFriedTwinkie, Outbuilding,TauntedHoRse, MechanicalBull, 
-	CableCar, PicnicArea, CircusTent, TiltAWhirl
-		};
+		tz_rooms = new Room[] {CottonCandyStand,MiniatureTrainStation,MiniatureTrainCars,ChairSwing,ChairSwingTower,ChairSwingSeat,ChairSwingUnderGrating,ChairSwingTowerMid,ChairSwingTowerNearTop,ChairSwingTowerOnTop,HiStriker,WaterGunGame,DeepFriedTwinkie,Outbuilding,TauntedHoRse,MechanicalBull,CableCar,PicnicArea,CircusTent,TiltAWhirl};
 
-		dt_rooms = new Room[] { Begin, 
-	FenceHole, Home, Work, Tracks, Fence, 
-	TieShoes, FenceLock, Tracks2, ShoesNoise, FreakOut, 
-	Melissa, TakeLoveLock, AskMelissa, MaintenanceShed, MaintenanceRearDoor, 
-	MaintenanceRearWindow, MaintenanceShedFront, MaintShedFrontDoor, MaintShedInside, TakeCrowbar, 
-	Gazebo, BurlesqueParlor, FerrisWheel, FunnelcakeStand, PublicSquare, 
-	ArcherySidestall,BasketballSidestall, HotDogCart, MerryGoRound, PortaJon, 
-	BumperCars, TicketBooth, BurlesqueStage
-		};
+		dt_rooms = new Room[] {Begin,FenceHole,Home,Work,Tracks,Fence,TieShoes,FenceLock,Tracks2,ShoesNoise,FreakOut,Melissa,TakeLoveLock,AskMelissa,MaintenanceShed,MaintenanceRearDoor,MaintenanceRearWindow,MaintenanceShedFront,MaintShedFrontDoor,MaintShedInside,TakeCrowbar,Gazebo,BurlesqueParlor,FerrisWheel,FunnelcakeStand,PublicSquare,ArcherySidestall,BasketballSidestall,HotDogCart,MerryGoRound,PortaJon,BumperCars,TicketBooth,BurlesqueStage};
 
-		items = new Item[] { iYourself, 
-	iTshirt, iLoveLocks, iLoveLockKey, iThingamabob,iCrowbar, 
-	iMaintShedFD, iTissue, iChairSwingGrating, iChairSwingDoor, iChairSwingDoorHandle, 
-	iChairSwingLever, iChairSwingTowerNearTopDoor, iFlashlight, iBurlesqueSofa, iBurlesqueSwitch,
-	iBurlesquePiano, iBurlesquePainting, iBurlesqueManikin
-		};
+		items = new Item[] {iYourself,iTshirt,iLoveLocks,iLoveLockKey,iThingamabob,iCrowbar,iMaintShedFD,iTissue,iChairSwingGrating,iChairSwingDoor,iChairSwingDoorHandle,iChairSwingLever,iChairSwingTowerNearTopDoor,iFlashlight,iBurlesqueSofa,iBurlesqueSwitch,iBurlesquePiano,iBurlesquePainting,iBurlesqueManikin};
 	}
 	
 	public static void changeFieldReferences(){ // bc Java has no iYourself**
@@ -42,25 +17,21 @@ for(Map.Entry<String,Room> entry : map.entrySet()) {
 	
 	public static void changeFieldReferences_tz() {
 		CottonCandyStand = tz_rooms[0];
-		
 		MiniatureTrainStation = tz_rooms[1];
 		MiniatureTrainCars = tz_rooms[2];
 		ChairSwing = tz_rooms[3];
 		ChairSwingTower = tz_rooms[4];
 		ChairSwingSeat = tz_rooms[5];
-
 		ChairSwingUnderGrating = tz_rooms[6];
 		ChairSwingTowerMid = tz_rooms[7];
 		ChairSwingTowerNearTop = tz_rooms[8];
 		ChairSwingTowerOnTop = tz_rooms[9];
 		HiStriker = tz_rooms[10];
-
 		WaterGunGame = tz_rooms[11];
 		DeepFriedTwinkie = tz_rooms[12];
 		Outbuilding = tz_rooms[13];
 		TauntedHoRse = tz_rooms[14];
 		MechanicalBull = tz_rooms[15];
-		
 		CableCar = tz_rooms[16];
 		PicnicArea = tz_rooms[17];
 		CircusTent = tz_rooms[18];
@@ -69,73 +40,60 @@ for(Map.Entry<String,Room> entry : map.entrySet()) {
 		
 	public static void changeFieldReferences_dt() {
 		Begin = dt_rooms[0];
-
 		FenceHole = dt_rooms[1];
 		Home = dt_rooms[2];
 		Work = dt_rooms[3];
 		Tracks = dt_rooms[4];
 		Fence = dt_rooms[5];
-
 		TieShoes = dt_rooms[6];
 		FenceLock = dt_rooms[7];
 		Tracks2 = dt_rooms[8];
 		ShoesNoise = dt_rooms[9];
 		FreakOut = dt_rooms[10];
-
 		Melissa = dt_rooms[11];
 		TakeLoveLock = dt_rooms[12];
 		AskMelissa = dt_rooms[13];
 		MaintenanceShed = dt_rooms[14];
 		MaintenanceRearDoor = dt_rooms[15];
-
 		MaintenanceRearWindow = dt_rooms[16];
 		MaintenanceShedFront = dt_rooms[17];
 		MaintShedFrontDoor = dt_rooms[18];
 		MaintShedInside = dt_rooms[19];
 		TakeCrowbar = dt_rooms[20];
-
 		Gazebo = dt_rooms[21];
 		BurlesqueParlor = dt_rooms[22];
 		FerrisWheel = dt_rooms[23];
 		FunnelcakeStand = dt_rooms[24];
 		PublicSquare = dt_rooms[25];
-
 		ArcherySidestall = dt_rooms[26];
 		BasketballSidestall = dt_rooms[27];
 		HotDogCart = dt_rooms[28];
 		MerryGoRound = dt_rooms[29];
 		PortaJon = dt_rooms[30];
-
 		BumperCars = dt_rooms[31];
 		TicketBooth = dt_rooms[32];
-		BurlesqueStage  = dt_rooms[33];
+		BurlesqueStage = dt_rooms[33];
 	}
 	
 	public static void changeFieldReferences_items() {
 		iYourself = items[0];
-		
-		iTshirt = items[1]; 
-		iLoveLocks = items[2]; 
-		iLoveLockKey = items[3]; 
+		iTshirt = items[1];
+		iLoveLocks = items[2];
+		iLoveLockKey = items[3];
 		iThingamabob = items[4];
-		iCrowbar = items[5]; 
-		
-		iMaintShedFD = items[6]; 
-		iTissue = items[7]; 
-		iChairSwingGrating = items[8]; 
-		iChairSwingDoor = items[9]; 
+		iCrowbar = items[5];
+		iMaintShedFD = items[6];
+		iTissue = items[7];
+		iChairSwingGrating = items[8];
+		iChairSwingDoor = items[9];
 		iChairSwingDoorHandle = items[10];
-		
 		iChairSwingLever = items[11];
 		iChairSwingTowerNearTopDoor = items[12];
 		iFlashlight = items[13];
 		iBurlesqueSofa = items[14];
 		iBurlesqueSwitch = items[15];
-		
 		iBurlesquePiano = items[16];
 		iBurlesquePainting = items[17];
 		iBurlesqueManikin = items[18];
 	}
-
-
 }
--- 2/makeBulkArrays.pl
+++ 2/makeBulkArrays.pl
@@ -1,4 +1,102 @@
-!#/usr/bin/perl
+#!/usr/bin/perl
 use warnings;
 use strict;
+
+our $num = 0;
+our $temp;
+our @dt;
+our $dt;
+our @items;
+our $items;
+our @tz;
+our $tz;
+
+	our $rooms = join "",<STDIN>;
+
+	#put tz rooms in array
+	if ($rooms =~ /\/\/TZ's rooms(.*)\/\/DT's rooms/is) { $temp = $1; }
+	else { print "no tz\n"; }
+	@tz = ($temp =~ /\s*?(\w+)[,;]/ig);
+	#print "Result = @tz\n\n";
+	
+	#put dt rooms in array
+	if ($rooms =~ /\/\/DT's rooms(.*)\/\/items/is) { $temp = $1; }
+	else { print "no dt\n"; }
+	@dt = ($temp =~ /\s*?(\w+)[,;]/ig);
+	#print "Result = @dt\n\n";
+	
+	#put items in array
+	if ($rooms =~ /\/\/items(.*)\/\/persons/is) { $temp = $1; }
+	else { print "no items\n"; }
+	@items= ($temp =~ /\s*?(\w+)[,;]/ig);
+	#print "Result = @items\n\n";
+	
+	
+	print "package dtz.cyoa;
+class Rooms_bulk extends Rooms {
+
+	public static void initBulkArrays(){
+		tz_rooms = new Room[] {";
+		
+	for (my $i=0;$i<$#tz;$i++){
+		print "$tz[$i],";
+	}
+	print "$tz[$#tz]";
+	
+	print "};
+
+		dt_rooms = new Room[] {";
+	
+	for (my $i=0;$i<$#dt;$i++){
+		print "$dt[$i],";
+	}
+	print "$dt[$#dt]";	
+
+	print "};
+
+		items = new Item[] {";
+	
+	for (my $i=0;$i<$#items;$i++){
+		print "$items[$i],";
+	}
+	print "$items[$#items]";
+	
+	print "};
+	}
+	
+	public static void changeFieldReferences(){ // bc Java has no iYourself**
+		changeFieldReferences_tz();
+		changeFieldReferences_dt();
+		changeFieldReferences_items();
+	}
+	
+	public static void changeFieldReferences_tz() {\n";
+	
+	for (my $i=0;$i<=$#tz;$i++){
+		print "\t\t$tz[$i] = tz_rooms[$i];\n";
+	}
+	print "\t}
+		
+	public static void changeFieldReferences_dt() {\n";
+	
+	for (my $i=0;$i<=$#dt;$i++){
+		print "\t\t$dt[$i] = dt_rooms[$i];\n";
+	}
+	print "\t}
+	
+	public static void changeFieldReferences_items() {\n";
+	
+	for (my $i=0;$i<=$#items;$i++){
+		print "\t\t$items[$i] = items[$i];\n";
+	}
+	print "\t}\n}";
+	
+	
+	
+	
+	
+		
+		
+	
+	
 
